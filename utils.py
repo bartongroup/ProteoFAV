ca3782949e38c0fb58e7bc921fa10ef1e3ed7a3f
tbrittoborges <tbritttoborges@dundee.ac.uk>
2015-07-03 14:03:33 +0100

    fixed merge_table bug.

--- utils.py
+++ utils.py
@@ -15,9 +15,10 @@ import numpy as np
 import requests
 
 from config import defaults, Defaults
-import to_table
+from to_table import _dssp_to_table, _sifts_residues_to_table, _mmcif_atom_to_table
 
 log = logging.getLogger(__name__)
+
 
 def current_date():
     """
@@ -261,13 +262,19 @@ def _fetch_sifts_best(uniprot_id, first=False):
 
 
 def merge_tables(uniprot_id, pdb_id=None, chain=None, groupby='CA',
-                 defaults=None):
+                 defaults=None, cols=None):
     """
     Merges the output from multiple to table method.
     if no pdb_id uses sifts_best_structure
     if no chain uses first
     or if use_all_chains use all chains of the pdb_id
     """
+    if cols:
+        for col in ('PDB_dbResNum', 'PDB_dbChainId'):
+            if col not in cols:
+                if not isinstance(cols, list):
+                    cols = list(cols)
+                cols.append(col)
 
     def to_list(series):
         return series.tolist()
@@ -313,23 +320,25 @@ def merge_tables(uniprot_id, pdb_id=None, chain=None, groupby='CA',
     dssp_path = path.join(defaults.db_dssp, pdb_id + ".dssp")
     sifts_path = path.join(defaults.db_sifts, pdb_id + ".xml")
 
-    cif_table = to_table._mmcif_atom_to_table(cif_path)
+    cif_table = _mmcif_atom_to_table(cif_path)
     cif_table = cif_table.query("label_asym_id == @chain & group_PDB == 'ATOM'")
     # next line raises a SettingWithCopyWarning but seems to be correct
-    cif_table.loc[:, ("label_seq_id")] = cif_table.loc[:, "label_seq_id"].astype(np.int)
+    # TODO review next line.
+    cif_table.loc[:, "label_seq_id"] = cif_table.loc[:, "label_seq_id"].astype(np.int)
+
     # TODO keep heteroatoms?
 
-    dssp_table = to_table._dssp_to_table(dssp_path)
+    dssp_table = _dssp_to_table(dssp_path)
     dssp_table = dssp_table.query('chain_id == @chain')
     dssp_table.set_index(['icode'], inplace=True)
 
-    sifts_table = to_table._sifts_residues_to_table(sifts_path)
+    sifts_table = _sifts_residues_to_table(sifts_path, cols=cols)
     sifts_table = sifts_table.query('PDB_dbChainId == @chain')
     sifts_table.set_index(['PDB_dbResNum'], inplace=True)
 
     if groupby == 'CA':
         cif_table = cif_table.query("label_atom_id == 'CA'")
-    cif_table = cif_table.groupby('label_seq_id').agg(groupby_opts[groupby])
+    cif_table = cif_table.groupby('auth_seq_id').agg(groupby_opts[groupby])
 
     # This will remove all atoms annotated in sifts but not in mmcif seqres
     # To keep those, sift_table should be the left in join
