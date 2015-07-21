3026ffcabb2aa8e17e6dd7d4e7eabab9851607db
tbrittoborges <tbritttoborges@dundee.ac.uk>
2015-07-21 17:46:19 +0100

    changed query function to verbose select.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
+import logging
 
 from os import path
 import numpy as np
@@ -7,6 +8,7 @@ import numpy as np
 from to_table import _dssp_to_table, _sifts_residues_to_table, _mmcif_atom_to_table, pd
 from utils import _fetch_sifts_best
 
+log = logging.getLogger(__name__)
 
 def merge_tables(uniprot_id=None, pdb_id=None, chain=None, groupby='CA', defaults=None, cols=None,
                  model=1):
@@ -37,7 +39,6 @@ def merge_tables(uniprot_id=None, pdb_id=None, chain=None, groupby='CA', default
 
     def to_unique(series):
         return series.unique()
-
     if not defaults:
         from config import defaults
 
@@ -60,12 +61,13 @@ def merge_tables(uniprot_id=None, pdb_id=None, chain=None, groupby='CA', default
                     'SC': {'label_comp_id': to_unique,
                            'label_atom_id': to_unique,
                            'label_asym_id': to_unique,
-                           'Cartn_x': "mean",
-                           'Cartn_y': "mean",
-                           'Cartn_z': "mean",
-                           'occupancy': "mean",
-                           'B_iso_or_equiv': "mean"},
-                    'centroid':{}} # TODO
+                           'Cartn_x': 'mean',
+                           'Cartn_y': 'mean',
+                           'Cartn_z': 'mean',
+                           'occupancy': 'mean',
+                           'B_iso_or_equiv': 'mean'},
+                    'centroid':{},
+                    'all_atoms':{}} # TODO
 
     try:
         groupby_opts[groupby]
@@ -81,57 +83,56 @@ def merge_tables(uniprot_id=None, pdb_id=None, chain=None, groupby='CA', default
         best_pdb = _fetch_sifts_best(uniprot_id)[pdb_id]
         chain = best_pdb['chain_id']
 
-    cif_path = path.join(defaults.db_mmcif, pdb_id + ".cif")
-    dssp_path = path.join(defaults.db_dssp, pdb_id + ".dssp")
-    sifts_path = path.join(defaults.db_sifts, pdb_id + ".xml")
+    cif_path = path.join(defaults.db_mmcif, pdb_id + '.cif')
+    dssp_path = path.join(defaults.db_dssp, pdb_id + '.dssp')
+    sifts_path = path.join(defaults.db_sifts, pdb_id + '.xml')
 
     cif_table = _mmcif_atom_to_table(cif_path)
     try:
-        cif_table = cif_table.query(
-            "pdbx_PDB_model_num == @model & label_asym_id == @chain & group_PDB == 'ATOM'")
-    except pd.computation.ops.UndefinedVariableError:
-        # TODO log
-        cif_table = cif_table.query("label_asym_id == @chain & group_PDB == 'ATOM'")
+        cif_table = cif_table[cif_table.pdbx_PDB_model_num == model &
+                              cif_table.label_asym_id == chain &
+                              cif_table.group_PDB == 'ATOM']
+    except AttributeError:
+        err = 'Structure {} has only one model'.format
+        log.info(err(pdb_id))
+        cif_table = cif_table[cif_table.label_asym_id == chain &
+                              cif_table.group_PDB == 'ATOM']
+
 
     # next line raises a SettingWithCopyWarning but seems to be correct
-    # TODO review next line.
-
-    cif_table.loc[:, "label_seq_id"] = cif_table.loc[:, "label_seq_id"].astype(np.int)
-
-    # TODO keep heteroatoms?
+    cif_table.loc[:, 'label_seq_id'] = cif_table.loc[:, 'label_seq_id'].astype(np.int)
 
     dssp_table = _dssp_to_table(dssp_path)
-    dssp_table = dssp_table.query('chain_id == @chain')
+    dssp_table = dssp_table[dssp_table.chain_id == chain]
     dssp_table.icode = dssp_table.icode.astype(np.int)
     dssp_table.set_index(['icode'], inplace=True)
 
     sifts_table = _sifts_residues_to_table(sifts_path)
-    sifts_table = sifts_table.query('PDB_dbChainId == @chain')
+    sifts_table = sifts_table[sifts_table.PDB_dbChainId == chain]
     sifts_table.PDB_dbResNum = sifts_table.PDB_dbResNum.astype(np.int)
     sifts_table.set_index(['PDB_dbResNum'], inplace=True)
 
     if groupby == 'CA':
-        cif_table = cif_table.query("label_atom_id == 'CA'")
+        cif_table = cif_table[cif_table.label_atom_id == 'CA']
         # assert cif_table.label_seq_id is a sequence with number of aas
     elif groupby == 'SC':
-        # TODO substitute for is not in [...]
-        cif_table = cif_table[~cif_table.label_atom_id.isin(["CA", "C", "O", "N"])]
+        cif_table = cif_table[~cif_table.label_atom_id.isin(['CA', 'C', 'O', 'N'])]
         # assert cif_table.label_seq_id is a sequence with number of aas
 
     cif_table = cif_table.groupby('auth_seq_id').agg(groupby_opts[groupby])
 
-    # This will remove all atoms annotated in sifts but not in mmcif seqres
-    # To keep those, sift_table should be the left in join
-    cif_sifts = cif_table.join(sifts_table)
-    cif_sifts_dssp = cif_sifts.join(dssp_table)
+    # Sift data in used as left element int the join, because MMCIF atom lines and DSSP
+    # Ignore not observed residues.
+    sifts_cif = sifts_table.join(cif_table)
+    cif_sifts_dssp = sifts_cif.join(dssp_table)
     return cif_sifts_dssp
 
 if __name__ == '__main__':
     from config import Defaults
-    defaults = Defaults("config.txt")
-    defaults.db_mmcif = "tests/CIF"
-    defaults.db_dssp = "tests/DSSP"
-    defaults.db_sifts = "tests/SIFTS"
+    defaults = Defaults('config.txt')
+    defaults.db_mmcif = 'tests/CIF'
+    defaults.db_dssp = 'tests/DSSP'
+    defaults.db_sifts = 'tests/SIFTS'
 
-    X = merge_tables(pdb_id="3edv", chain="B", defaults=defaults)
+    X = merge_tables(pdb_id='3edv', chain='B', defaults=defaults)
     pass
