b01e33ec5fc4075a6ac11b30b90a977265eb9f61
tbrittoborges <tbrittoborges@dundee.ac.uk>
2015-08-10 18:21:23 +0100

    Local instead global (default var).

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 from to_table import _dssp_to_table, _sifts_residues_to_table, \
     _mmcif_atom_to_table
 from utils import _fetch_sifts_best, three_to_single_aa
+from config import defaults
 
 log = logging.getLogger(__name__)
 
@@ -37,7 +38,8 @@ def merge_tables(uniprot_id=None, pdb_id=None, chain=None, groupby='CA',
     to_unique = lambda series: series.unique()
 
     if not default:
-        from config import defaults as default
+        default = defaults
+
 
     groupby_opts = {'list': {'label_comp_id': 'unique',
                              'label_atom_id': to_list,
