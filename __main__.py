98fdd78770ab0c938330ceed89b5e8d74062e3be
tbrittoborges <tbrittoborges@dundee.ac.uk>
2015-10-07 09:24:17 +0100

    inital commit.

--- __main__.py
+++ __main__.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
-
-from __future__ import absolute_import
+"""
+Command line application of the library.
+"""
+import argparse
 import logging
+from main import merge_tables
 
 log = logging.getLogger(__name__)
+usage = ''
+parser = argparse.ArgumentParser(description=__doc__, usage=usage)
+parser.add_argument('--pdb_acc', type=str)
+parser.add_argument('--chain')
+parser.add_argument('output')
 
-if __name__ == '__main__':
-    pass
+args = parser.parse_args()
+table = merge_tables(pdb_id=args.pdb_acc, chain=args.chain)
+table.to_csv(args.output)
