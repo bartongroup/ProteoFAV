ce46d15a3bf7b1c59a66c072d868c7e97953ca66
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2015-10-20 17:03:56 +0100

    Basic functionality to display a PDB structure in PyMol with highlighted variants

---
+++ visualise.py
@@ -0,0 +1,34 @@
+__author__ = 'smacgowan'
+
+import pymol
+import pandas as pd
+
+from main import merge_tables
+
+
+def visualise(pdb_id):
+
+    # Open the PDB with pymol
+    pymol.finish_launching()
+    pymol.cmd.fetch(pdb_id)
+    pymol.cmd.hide("everything", "all")
+    pymol.cmd.show("ribbon", "all")
+    pymol.util.cbc()
+
+    # Get available variants for any chain
+    chains = pymol.cmd.get_chains(pdb_id)
+
+    # Get the variants
+    for chain in chains:
+        residue_mappings = merge_tables(pdb_id=pdb_id, chain=chain, add_variants=True)
+        start = residue_mappings.start
+        variant_residues = list(start[pd.notnull(start)].astype(int).astype(str).unique())
+
+        # Create the selection
+        selection = 'chain ' + chain + ' and resi ' + '+'.join(variant_residues)
+        name = 'chain_' + chain + '_vars'
+        pymol.cmd.select(name, selection)
+
+        # Apply some styles
+        pymol.cmd.show("sticks", name)
+
