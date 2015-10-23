28879f15e52c63673257add663825693d3a204ff
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2015-10-23 17:06:40 +0100

    Added ability to stratify variants by disease in PyMol visualisation using either UniProt or EnsEMBL annotations

--- visualise.py
+++ visualise.py
@@ -2,15 +2,36 @@ __author__ = 'smacgowan'
 
 import pymol
 import pandas as pd
+import re
 
 from main import merge_tables
 
+from to_table import _variant_characteristics_from_identifiers
+from to_table import _fetch_uniprot_variants
 
-def visualise(pdb_id):
+
+def visualise(pdb_id, assembly=False, group_by_trait=False, use_ensembl=False, use_uniprot=False):
+    """
+
+    :param pdb_id:
+    :param group_by_trait:
+    :param use_ensembl:
+    :param use_uniprot:
+    :return:
+    """
+
+    if group_by_trait:
+        if not use_ensembl != use_uniprot and not any([use_ensembl, use_uniprot]):
+            raise TypeError("One of the following must be True if grouping by traits:"
+                            "use_ensembl or use_uniprot")
 
     # Open the PDB with pymol
     pymol.finish_launching()
-    pymol.cmd.fetch(pdb_id)
+    if assembly:
+        pymol.cmd.fetch(pdb_id, type='pdb1')
+        pymol.cmd.set('all_states', 'on')
+    else:
+        pymol.cmd.fetch(pdb_id)
     pymol.cmd.hide("everything", "all")
     pymol.cmd.show("ribbon", "all")
     pymol.util.cbc()
@@ -18,8 +39,9 @@ def visualise(pdb_id):
     # Get available variants for any chain
     chains = pymol.cmd.get_chains(pdb_id)
 
-    # Get the variants
     for chain in chains:
+
+        # Get the variants for this chain
         residue_mappings = merge_tables(pdb_id=pdb_id, chain=chain, add_variants=True)
         has_variant = residue_mappings.start.notnull()
         start = residue_mappings.PDB_dbResNum[has_variant]
@@ -31,6 +53,73 @@ def visualise(pdb_id):
         pymol.cmd.select(name, selection)
 
         # Apply some styles
-        pymol.cmd.show("sticks", name)
+        pymol.cmd.show("lines", name)
         pymol.util.cnc(name)
 
+
+        if group_by_trait and use_ensembl:
+            variant_ids = residue_mappings.id_y[has_variant]
+
+            # For now, need to iterate with GET requests until POST can retrieve phenotypes
+            traits = []
+            for variant in variant_ids:
+                phenos = _variant_characteristics_from_identifiers(str(variant))['phenotypes']
+                for entry in phenos:
+                    traits.append([variant, entry['trait']])
+
+            groups = set(zip(*traits)[1])
+            for group in groups:
+                variants_in_group = []
+                for id, trait in traits:
+                    if trait == group:
+                        variants_in_group.append(id)
+
+                # Get the relevant residues and create selection for current group
+                in_group = [ True if x in variants_in_group else False for x in residue_mappings.id_y ]
+                start = residue_mappings.PDB_dbResNum[in_group]
+                variant_residues = list(start[pd.notnull(start)].astype(int).astype(str).unique())
+
+                # sanitisation of the selection name for pymol is important or the name existence test will always
+                # fail and we'll over write entries from previous chains!
+                name = group + '_vars'
+                name = re.sub("[\W\d]+", "_", name.strip())
+
+                if name not in pymol.cmd.get_names('selections'):
+                    pymol.cmd.select(name, 'none')
+
+                selection = '(chain ' + chain + ' and resi ' + '+'.join(variant_residues) + ') or ' + name
+                #name = group + '_vars'
+                print name, selection
+                pymol.cmd.select(name, selection)
+
+        if group_by_trait and use_uniprot:
+
+            # Extract first uniprot (REFACTOR THIS, also `merge_tables`)
+            structure_uniprots = residue_mappings.UniProt_dbAccessionId
+            structure_uniprots = structure_uniprots[structure_uniprots.notnull()]
+            structure_uniprot = structure_uniprots.unique()[0]
+
+            # Get the variants and merge onto residue_mappings
+            uniprot_vars = _fetch_uniprot_variants(structure_uniprot)
+            mapped = pd.merge(residue_mappings, uniprot_vars,
+                              left_on='UniProt_dbResNum', right_on='resi',
+                              how='inner') # Not ideal as UniProt_dbResNum is not unique but is fit for purpose
+
+            # Create a selection for each trait
+            groups = mapped.disease.unique()
+            for group in groups:
+                in_group = mapped.disease == group
+
+                # The following is copied from the above with `residue_mappings` change to `mapped`
+                start = mapped.PDB_dbResNum[in_group]
+                variant_residues = list(start[pd.notnull(start)].astype(int).astype(str).unique())
+
+                name = group.replace(' ', '_') + '_vars'
+                if name not in pymol.cmd.get_names('selections'):
+                    pymol.cmd.select(name, 'none')
+
+                selection = 'chain ' + chain + ' and resi ' + '+'.join(variant_residues) + ' or ' + name
+                #name = group + '_vars'
+                print name, selection
+                pymol.cmd.select(name, selection)
+                # END of copy
