835562ada984235f3ee9a4a0e37374d5852b0f74
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2015-11-12 15:48:11 +0000

    3D plots in cluster output instead of 2D

--- clustering.py
+++ clustering.py
@@ -10,6 +10,7 @@ from main import merge_tables
 from to_table import _fetch_uniprot_variants
 import pandas as pd
 from scipy.spatial.distance import pdist
+from mpl_toolkits.mplot3d import Axes3D
 
 # Fetch raw data
 structure = merge_tables(pdb_id='3tnu', chain='A')  ## Don't add variants yet!
@@ -24,21 +25,24 @@ xyz = np.transpose(xyz)
 d = pdist(xyz)
 a = d
 
-fig, axes23 = plt.subplots(2, 3)
+fig = plt.figure(figsize=plt.figaspect(3./4.))
 
-for method, axes in zip(['single', 'complete'], axes23):
+for method, row in zip(['single', 'complete'], [0, 3]):
     z = hac.linkage(a, method=method)
 
     # Plotting
-    axes[0].plot(range(1, len(z)+1), z[::-1, 2])
+    axes = []
+    ax = fig.add_subplot(2, 3, 1 + row)
+    axes.append(ax)
+    ax.plot(range(1, len(z)+1), z[::-1, 2])
     knee = np.diff(z[::-1, 2], 2)
-    axes[0].plot(range(2, len(z)), knee)
+    ax.plot(range(2, len(z)), knee)
 
     num_clust1 = knee.argmax() + 2
     knee[knee.argmax()] = 0
     num_clust2 = knee.argmax() + 2
 
-    axes[0].text(num_clust1, z[::-1, 2][num_clust1-1], 'possible\n<- knee point')
+    ax.text(num_clust1, z[::-1, 2][num_clust1-1], 'possible\n<- knee point')
 
     part1 = hac.fcluster(z, num_clust1, 'maxclust')
     part2 = hac.fcluster(z, num_clust2, 'maxclust')
@@ -46,10 +50,12 @@ for method, axes in zip(['single', 'complete'], axes23):
     clr = ['#2200CC' ,'#D9007E' ,'#FF6600' ,'#FFCC00' ,'#ACE600' ,'#0099CC' ,
     '#8900CC' ,'#FF0000' ,'#FF9900' ,'#FFFF00' ,'#00CC01' ,'#0055CC']
 
-    for part, ax in zip([part1, part2], axes[1:]):
+    for part, i in zip([part1, part2], [2, 3]):
+        ax = fig.add_subplot(2, 3, i+row, projection='3d')
+        axes.append(ax)
         for cluster in set(part):
             ax.scatter(xyz[part == cluster, 0], xyz[part == cluster, 1],
-                       color=clr[cluster - 1])
+                       xyz[part == cluster, 2], c=clr[cluster - 1])
 
     m = '\n(method: {})'.format(method)
     plt.setp(axes[0], title='Screeplot{}'.format(m), xlabel='partition',
@@ -58,4 +64,5 @@ for method, axes in zip(['single', 'complete'], axes23):
     plt.setp(axes[2], title='{} Clusters'.format(num_clust2))
 
 plt.tight_layout()
+fig.set_tight_layout(True)
 plt.show()
