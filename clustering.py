64796232b9df5f049f6002c83060a80c95ecc8b9
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2015-11-13 10:46:33 +0000

    Wrapped script components in functions and added main block

--- clustering.py
+++ clustering.py
@@ -13,66 +13,73 @@ from scipy.spatial.distance import pdist
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial import ConvexHull, Delaunay
 
-# Fetch raw data
-structure = merge_tables(pdb_id='3tnu', chain='A')  ## Don't add variants yet!
-variants = _fetch_uniprot_variants('P02533')
-# Merge these
-structure.UniProt_dbResNum = structure.UniProt_dbResNum.astype('float')
-merged = pd.merge(structure, variants, on='UniProt_dbResNum')
-# Build array distance matrix
-merged_real = merged[np.isfinite(merged['Cartn_x'])]
-xyz = np.array([merged_real.Cartn_x, merged_real.Cartn_y, merged_real.Cartn_z])
-xyz = np.transpose(xyz)
-d = pdist(xyz)
-a = d
+def variant_distances(pdb_id, chain, uniprot_id):
+    # Fetch raw data
+    structure = merge_tables(pdb_id=pdb_id, chain=chain)  ## Don't add variants yet!
+    variants = _fetch_uniprot_variants(uniprot_id)
+    # Merge these
+    structure.UniProt_dbResNum = structure.UniProt_dbResNum.astype('float')
+    merged = pd.merge(structure, variants, on='UniProt_dbResNum')
+    # Build array distance matrix
+    merged_real = merged[np.isfinite(merged['Cartn_x'])]
+    xyz = np.array([merged_real.Cartn_x, merged_real.Cartn_y, merged_real.Cartn_z])
+    xyz = np.transpose(xyz)
+    return pdist(xyz), xyz
 
-fig, axes23 = plt.subplots(2, 3)
 
-for method, axes, offset in zip(['single', 'complete'], axes23, [0, 3]):
-    z = hac.linkage(a, method=method)
+def linkage_cluster(a, xyz):
+    fig, axes23 = plt.subplots(2, 3)
 
-    # Plotting
-    axes[0].plot(range(1, len(z)+1), z[::-1, 2])
-    knee = np.diff(z[::-1, 2], 2)
-    axes[0].plot(range(2, len(z)), knee)
+    for method, axes, offset in zip(['single', 'complete'], axes23, [0, 3]):
+        z = hac.linkage(a, method=method)
 
-    num_clust1 = knee.argmax() + 2
-    knee[knee.argmax()] = 0
-    num_clust2 = knee.argmax() + 2
+        # Plotting
+        axes[0].plot(range(1, len(z)+1), z[::-1, 2])
+        knee = np.diff(z[::-1, 2], 2)
+        axes[0].plot(range(2, len(z)), knee)
 
-    axes[0].text(num_clust1, z[::-1, 2][num_clust1-1], 'possible\n<- knee point')
+        num_clust1 = knee.argmax() + 2
+        knee[knee.argmax()] = 0
+        num_clust2 = knee.argmax() + 2
 
-    if num_clust1 > 12:
-        num_clust1 = 12
-    if num_clust2 > 12:
-        num_clust2 = 12
+        axes[0].text(num_clust1, z[::-1, 2][num_clust1-1], 'possible\n<- knee point')
 
-    part1 = hac.fcluster(z, num_clust1, 'maxclust')
-    part2 = hac.fcluster(z, num_clust2, 'maxclust')
+        if num_clust1 > 12:
+            num_clust1 = 12
+        if num_clust2 > 12:
+            num_clust2 = 12
 
-    clr = ['#2200CC' ,'#D9007E' ,'#FF6600' ,'#FFCC00' ,'#ACE600' ,'#0099CC' ,
-    '#8900CC' ,'#FF0000' ,'#FF9900' ,'#FFFF00' ,'#00CC01' ,'#0055CC']
+        part1 = hac.fcluster(z, num_clust1, 'maxclust')
+        part2 = hac.fcluster(z, num_clust2, 'maxclust')
 
-    for part, i in zip([part1, part2], [2, 3]):
-        ax = fig.add_subplot(2, 3, i + offset, projection='3d')
-        for cluster in set(part):
-            ax.scatter(xyz[part == cluster, 0], xyz[part == cluster, 1],
-                       xyz[part == cluster, 2], c=clr[cluster - 1])
-            # Compute and plot the Point Cloud Complex Hull
-            points = xyz[part == cluster]
-            if len(points) >= 4:
-                hull = ConvexHull(points)
-                for simplex in hull.simplices:
-                    ax.plot_wireframe(points[simplex, 0], points[simplex, 1],
-                                      points[simplex, 2], color=clr[cluster - 1])
+        clr = ['#2200CC' ,'#D9007E' ,'#FF6600' ,'#FFCC00' ,'#ACE600' ,'#0099CC' ,
+        '#8900CC' ,'#FF0000' ,'#FF9900' ,'#FFFF00' ,'#00CC01' ,'#0055CC']
 
-    m = '\n(method: {})'.format(method)
-    plt.setp(axes[0], title='Screeplot{}'.format(m), xlabel='partition',
-             ylabel='{}\ncluster distance'.format(m))
-    plt.setp(axes[1], title='{} Clusters'.format(num_clust1))
-    plt.setp(axes[2], title='{} Clusters'.format(num_clust2))
-    axes[1].axis('off')
-    axes[2].axis('off')
+        for part, i in zip([part1, part2], [2, 3]):
+            ax = fig.add_subplot(2, 3, i + offset, projection='3d')
+            for cluster in set(part):
+                ax.scatter(xyz[part == cluster, 0], xyz[part == cluster, 1],
+                           xyz[part == cluster, 2], c=clr[cluster - 1])
+                # Compute and plot the Point Cloud Complex Hull
+                points = xyz[part == cluster]
+                if len(points) >= 4:
+                    hull = ConvexHull(points)
+                    for simplex in hull.simplices:
+                        ax.plot_wireframe(points[simplex, 0], points[simplex, 1],
+                                          points[simplex, 2], color=clr[cluster - 1])
 
-plt.tight_layout()
-plt.show()
+        m = '\n(method: {})'.format(method)
+        plt.setp(axes[0], title='Screeplot{}'.format(m), xlabel='partition',
+                 ylabel='{}\ncluster distance'.format(m))
+        plt.setp(axes[1], title='{} Clusters'.format(num_clust1))
+        plt.setp(axes[2], title='{} Clusters'.format(num_clust2))
+        axes[1].axis('off')
+        axes[2].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    d, points = variant_distances(pdb_id='3ecr', chain='A', uniprot_id='P08397')
+    linkage_cluster(d, points)
