bd99e685671f67628615d445ee8cae0880222bde
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2015-11-18 15:38:15 +0000

    Added main block to visualise module

--- visualise.py
+++ visualise.py
@@ -139,3 +139,7 @@ def visualise(pdb_id, assembly=False, use_ensembl=False, use_uniprot=False):
                 in_group = mapped.disease == group
 
                 build_selection(chain, group, in_group, mapped)
+
+
+if __name__ == '__main__':
+    visualise('3tnu', assembly=True, use_uniprot=True)
