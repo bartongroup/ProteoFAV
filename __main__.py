335328f0f790f1193ef315e1cb4e12519654c04a
tbrittoborges <tbrittoborges@dundee.ac.uk>
2015-11-30 00:27:09 +0000

    add new parameter for the command line interface.

--- __main__.py
+++ __main__.py
@@ -12,12 +12,20 @@ from main import merge_tables
 
 log = logging.getLogger(__name__)
 usage = ''
-# TODO select log level
 parser = argparse.ArgumentParser(description=__doc__, usage=usage)
 parser.add_argument('--pdb', type=str)
 parser.add_argument('--chain')
+parser.add_argument('--sp_accession')
+parser.add_argument('output', required=True)
+
+parser.add_argument('-v', '--verbose', action='store_true')
+parser.add_argument('-q', '--quiet', action='store_true')
+parser.add_argument('-l', '--log') # set log to file.
+
 parser.add_argument('--add_annotation', action='store_true')
-parser.add_argument('output')
+parser.add_argument('--add_validation', action='store_true')
+parser.add_argument('--add_variants', action='store_true')
+parser.add_argument('--add_annotations', action='store_true')
 
 args = parser.parse_args()
 table = merge_tables(pdb_id=args.pdb, chain=args.chain,
