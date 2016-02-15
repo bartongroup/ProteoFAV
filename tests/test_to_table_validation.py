e0b84d0d5e6e5f693ba05fc8ba83a93a87cbc9c3
Fábio Madeira <fbiomadeira@gmail.com>
2016-02-15 17:36:35 +0000

    Refactoring

--- tests/test_to_table_validation.py
+++ tests/test_to_table_validation.py
@@ -14,7 +14,7 @@ class TestValidationParser(unittest.TestCase):
     def setUp(self):
         """Initialize the framework for testing."""
         self.path_2pah = path.join(path.dirname(__file__),
-                                   "validation_xml/2pah_validation.xml")
+                                   "VALIDATION/2pah_validation.xml")
         self.parser = _pdb_validation_to_table
 
     def tearDown(self):
