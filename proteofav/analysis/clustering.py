fb3412dd79d44a8e40358abc4d0ac6972e414f4a
Thiago Britto Borges <tbrittoborges@dundee.ac.uk>
2016-06-13 15:06:43 +0100

    Clean up the group association function

--- proteofav/analysis/clustering.py
+++ proteofav/analysis/clustering.py
@@ -17,6 +17,75 @@ log = logging.getLogger(__name__)
 __author__ = 'smacgowan, tbrittoborges'
 
 
+def generate_chimera_attrFile(column):
+    """Write a chimera attribute fil
+    :param pd.Series column: column with attributes
+
+    ..example:
+    generate_chimera_attrFile(variants.applymap(counts).tcga)
+    """
+    with open('{}.txt'.format(column.name), 'w') as open_f:
+        open_f.write(
+            'attribute: {}\nmatch mode: 1-to-1\nrecipient: residues\n'.format(column.name))
+        open_f.write(column.reset_index()
+                     .to_string(index=False, header=False,
+                                formatters=["\t:{}".format, "\t{}".format]).
+                     replace(" ", ""))
+
+
+def counts(x):
+    """
+    Count elements inside column of iterable
+
+    :param object x:
+    :return int: number of elements per
+    """
+    try:
+        return len(x)
+    except TypeError:
+        return 0
+
+
+def cluster_significance(uniprot_id, column='somatic_variants'):
+    # cluster significance experiment
+    # variants_cols=None cluster which variants
+    # n_samples=10 number of times to bootstrap
+    # project_name where to save the files
+
+    # create a directory
+
+    # load the structure table and variants
+
+    # TODO use atoms=None and centroid position
+    """
+
+    :param uniprot_id:
+    :param column:
+    :return:
+    """
+    table = proteofav.merge_tables(uniprot_id=uniprot_id)
+    variants = proteofav.variants.select_variants(uniprot_id, column)
+    table.UniProt_dbResNum = table.UniProt_dbResNum.astype(float)
+    table = table.merge(variants, left_on='UniProt_dbResNum', right_index=True, how='left')
+
+    # fall back to PDB reference
+    table.set_index('PDB_dbResNum', inplace=True)
+
+    # multiple variants in the same residue are represented as multiple nodes in the same location
+    # TODO past version also supported clustering residues with/out variants
+    temp = pd.DataFrame(table[column].tolist(),
+                        index=table.index)
+    temp = temp.stack()
+    temp.index = temp.index.droplevel(1)
+    temp.name = column
+    table = table.drop([column], axis=1).join(temp)
+
+    return clustering(table, column)
+    #     for i in range(n_samples):
+    #         temp = variants.copy().sample(frac=1, replace=True)
+    #         temp.index = variants.index
+
+
 def distance_to_similarity(distance, method='max_minus_d', threshold=10., gamma=1. / 3):
     """
     Transform distance matrix (or array) to similarity by normalisation.
@@ -69,55 +138,32 @@ def mcl_clustering(similarity):
         except subprocess.CalledProcessError as e:
             log.error(e, exc_info=True)
 
-        clusters = [line.rstrip().split('\t') for line in output_file.readlines()]
-
-    # this could not be more obfuscated
-    clusters = pd.DataFrame(clusters)
-    clusters = clusters.stack()
-    clusters.index = clusters.index.droplevel(1)
-    clusters = pd.Series(clusters.index.astype(int), index=clusters.astype(int).values)
-    clusters.name = 'cluster_id'
-    return clusters
+        return [line.rstrip().split('\t') for line in output_file.readlines()]
 
 
-def clustering(table, column):
-    """
+def clustering(table, column, clustering_method=mcl_clustering):
+    """ Wrapper to support multiple clustering methods
 
-    :param pd.DataFrame table: Merged Table
-    :param str column: Name of the column to be clustered
+    :param function clustering_method: clustering method
+    :param pd.DataFrame table: table from ProteoFAV.merge_tables
+    :param str column: name of the column to be clustered
     :return:
     """
-    table = table.copy()
+    variant_3D = table.copy().loc[
+        table['Cartn_x'].notnull() & table[column].notnull(),
+        ['Cartn_x', 'Cartn_y', 'Cartn_z']]
 
     # First calculated the pairwise euclidean distance
-    distance = pdist(table.loc[table['Cartn_x'].notnull() & table[column].notnull(),
-                               ['Cartn_x', 'Cartn_y', 'Cartn_z']])
+    distance = pdist(variant_3D)
     similarity = squareform(distance_to_similarity(distance))
-    clusters = mcl_clustering(similarity)
-    return table.join(clusters)
-
-
-def cluster_significance(uniprot_id):
-    # variants_cols=None cluster which variants
-    # n_samples=10 number of times to bootstrap
-    # project_name where to save the files
-
-    # create a directory
-
-
-    table = proteofav.merge_tables(uniprot_id=uniprot_id)  # TODO use atoms=None and centroid posit
-    variants = proteofav.variants.select_variants(uniprot_id)
-    table.UniProt_dbResNum = table.UniProt_dbResNum.astype(float)
-    table = table.merge(variants, left_on='UniProt_dbResNum', right_index=True, how='left')
-    return table
-
-
-#     for i in range(n_samples):
-#         temp = variants.copy().sample(frac=1, replace=True)
-#         temp.index = variants.index
-
-
+    clusters = clustering_method(similarity)
+    # this wont work if a residues participates of two clusters, so we do it reversed to keep
+    # residues in their biggest cluster
+    for i, cluster_i in enumerate(reversed(clusters)):
+        cluster_i = [int(x) for x in cluster_i]
+        variant_3D.loc[variant_3D.index[cluster_i], 'cluster_id'] = i
+    return variant_3D
 
 
 if __name__ == '__main__':
-    pass
+    table = cluster_significance('O15294', column='ensembl_somatic')
